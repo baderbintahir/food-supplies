50435949abb9fa6e2caf277af203e3ff48364ce7
baderbintahir <badarbintahir@gmail.com>
2020-05-18 13:54:57 +0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+
+    render(){
+        return(
+            <div>Food Supplies</div>
+        )
+    }
+}
+
+export default App
